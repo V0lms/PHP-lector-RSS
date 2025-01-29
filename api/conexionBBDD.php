10bac8badd09cc645b07f0eca9dda97076fa2a5d
Your Name <you@example.com>
2025-01-29 14:40:24 +0100

    prueba8

--- api/conexionBBDD.php
+++ api/conexionBBDD.php
@@ -1,9 +1,9 @@
 <?php
 
 $Repit=false;
-$host="localhost";
-$user="root";
-$password="";
+$host="postgres://neondb_owner:npg_ito4ZxOywzF7@ep-autumn-feather-a25rwmok-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require";
+$user="neondb_owner";
+$password="npg_ito4ZxOywzF7";
 
 $link= mysqli_connect($host,$user,$password);
 $tildes=$link->query("SET NAMES 'utf8'");
