fd7f151f67a358ef3769c236e05e3c36be5edb99
Your Name <you@example.com>
2025-02-03 09:30:45 +0100

    prueba10

--- api/conexionBBDD.php
+++ api/conexionBBDD.php
@@ -1,8 +1,6 @@
 <?php
-
 $Repit=false;
-
 $link= mysqli_connect('postgres://neondb_owner:npg_do47QlhJBNcC@ep-holy-firefly-a26hg6mf-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require');
-$tildes=$link->query("SET NAMES 'utf8'");
+//$tildes=$link->query("SET NAMES 'utf8'");
 mysqli_select_db($link,'periodicos');
 
