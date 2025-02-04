de5266359963d07019d4ebf186f4d9c6e6f81ee8
Your Name <you@example.com>
2025-02-04 13:40:21 +0100

    qwert

--- api/conexionBBDD.php
+++ api/conexionBBDD.php
@@ -1,8 +1,7 @@
 <?php
 
 $Repit = false;
-$conn_string = "host=ep-holy-firefly-a26hg6mf.eu-central-1.aws.neon.tech dbname=neondb user=neondb_owner password=npg_do47QlhJBNcC";
-$link = pg_connect($conn_string);
+$link = pg_connect("host=ep-holy-firefly-a26hg6mf-pooler.eu-central-1.aws.neon.tech dbname=neondb user=neondb_owner password=npg_do47QlhJBNcC");
 
 if (!$link) {
     die('Error: ' . pg_last_error());
