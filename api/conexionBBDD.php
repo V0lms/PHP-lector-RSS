ca97b659f1b20928b8e8e83c356ab3600f9f79e8
Your Name <you@example.com>
2025-02-04 14:29:16 +0100

    ws

--- api/conexionBBDD.php
+++ api/conexionBBDD.php
@@ -1,12 +1,11 @@
 <?php
-$host = getenv('PGHOST');
-$db = getenv('PGDATABASE');
-$user = getenv('PGUSER');
-$password = getenv('PGPASSWORD');
-$sslmode = getenv('PGSSLMODE');
+$conn_str= "postgres://neondb_owner:npg_do47QlhJBNcC@ep-holy-firefly-a26hg6mf-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require";
 
-$link = pg_connect("host=$host dbname=$db user=$user password=$password sslmode=$sslmode");
+$link = pg_connect($conn_str);
 
 if (!$link) {
     die('Error de conexión: ' . pg_last_error());
 }
+
+pg_set_client_encoding($link,"UTF8");
+echo ".";
